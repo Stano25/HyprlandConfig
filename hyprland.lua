575e23e93b2926b5439d99bf9503e3b8374cc379
stano <stanovysocan@gmail.com>
2026-07-03 22:00:54 +0200

    Implemented default hyprland.lua config

--- hyprland.lua
+++ hyprland.lua
@@ -1,3 +1,13 @@
+------------------
+---- MONITORS ----
+------------------
+hl.monitor({
+    output   = "",
+    mode     = "preferred",
+    position = "auto",
+    scale    = "auto",
+})
+
 ---------------------
 ---- MY PROGRAMS ----
 ---------------------
@@ -18,7 +28,6 @@ hl.env("HYPRCURSOR_SIZE", "24")
 ---- AUTOSTART ----
 -------------------
 hl.on("hyprland.start", function ()
-  -- Vynúti nastavenie kurzora na pozadí plochy
   hl.exec_cmd("hyprctl setcursor Nordzy-cursors 24")
 end)
 
@@ -26,17 +35,87 @@ end)
 ---- INPUT ----
 ---------------
 hl.config({
-  input = {
-    kb_layout = "sk",
-    kb_variant = "",
-    -- Ponechané prázdne pre správnu funkčnosť pravého Altu (AltGr)
-    kb_options = "",
-  },
+    general = {
+            gaps_in  = 5,
+            gaps_out = 10,
+
+            border_size = 1,
+
+            col = {
+                active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+                inactive_border = "rgba(595959aa)",
+            },
+
+            -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+            resize_on_border = false,
+
+            -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+            allow_tearing = false,
+
+            layout = "dwindle",
+        },
+
+        decoration = {
+            rounding       = 5,
+            rounding_power = 2,
+
+            -- Change transparency of focused and unfocused windows
+            active_opacity   = 0.9,
+            inactive_opacity = 0.9,
+
+            shadow = {
+                enabled      = true,
+                range        = 4,
+                render_power = 3,
+                color        = 0xee1a1a1a,
+            },
+
+            blur = {
+                enabled   = true,
+                size      = 3,
+                passes    = 1,
+                vibrancy  = 0.1696,
+            },
+        },
+
+        animations = {
+            enabled = true,
+        },
 })
 
----------------------
----- KEYBINDINGS ----
----------------------
+----------------
+----  MISC  ----
+----------------
+
+hl.config({
+    misc = {
+        force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
+    },
+})
+
+
+---------------
+---- INPUT ----
+---------------
+hl.config({
+    input = {
+        kb_layout = "sk",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
+
+        follow_mouse = 1,
+
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = false,
+        },
+    },
+})
+
 
 -- Spúšťanie programov a správa okien
 hl.bind(mainMod .. " + S", hl.dsp.exec_cmd("ags toggle launcher"))
